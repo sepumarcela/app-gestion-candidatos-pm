716e55492e82a94d30b8f8e4423325d2221c0aa0
sepumarcela <yulimarce0616@gmail.com>
2026-03-17 20:59:39 -0500

    (chore) definición de rutas principales para la comunicación con la API generada desde el Back

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+const URL_BASE = "localhosto:8080/"
+export let end_points = {
+    // user: URL_BASE + "users"
+    users: `${URL_BASE}users`,
+    offers: `${URL_BASE}offers`,
+    candidates: `${URL_BASE}candidates`
+}
